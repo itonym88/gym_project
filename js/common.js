89c76b3153a94296c5e1eb85b46c2c22821009fb
itonym88 <mykhaylovanton@gmail.com>
2015-12-29 15:07:00 +0200

    front-end none responsive

--- js/common.js
+++ js/common.js
@@ -1,13 +1,37 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 		
-	// 	function heightDetect(){
-	// 	$(".main_head").css("height", $(window).height());
-	// };
-	// heightDetect();
-	// $(window).resize(function(){
-	// 	heightDetect();
-	// });
-});
+// var slides = $(".slider .slides").children(".slide");
+// var width = $(".slider .slides").width();
+// var i = slides.length;
+// var offset = i * width;
+// i--;
+
+// $(".slider .slides").css("width",offset);
+// offset = 0;
+// $("body .slider .next").click(function(){
+// if (offset < width * i) {
+// offset += width;
+// $(".slider .slides").css("transform","translate3d(-"+offset+"px, 0px, 0px)");
+// }
+// else{
+// 	offset=0;
+// 	$(".slider .slides").css("transform","translate3d(-"+offset+"px, 0px, 0px)");
+// }
+// });
+ 
+ 
+// $("body .slider .prev").click(function(){
+// if (offset > 0) {
+// offset -= width;
+// $(".slider .slides").css("transform","translate3d(-"+offset+"px, 0px, 0px)");
+// }
+// else{
+// 	offset=width*2;
+// 	$(".slider .slides").css("transform","translate3d(-"+offset+"px, 0px, 0px)");
+// }
+// });
+// });
+
 $(window).load(function(){
 	$(".loader_inner").fadeOut();
 	$(".loader").delay(400).fadeOut("slow");
